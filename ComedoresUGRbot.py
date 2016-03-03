10c3134a700cc628e6c423383deef85dbf837422
Alejandro Castilla <acastillaquesada@gmail.com>
2016-03-03 20:16:59 +0100

    Comments added to ComedoresUGRbot.py and some changes on README.md

--- ComedoresUGRbot.py
+++ ComedoresUGRbot.py
@@ -1,10 +1,24 @@
+# This code is written on Python 3, so make sure you are running that version.
+# The menu needs to be updated every week, so it's neccesary to restart it
+# manually or using cron on Linux, for example.
+
+# Import all the neccesary stuff. You need to install BeautifulSoup (python library) on your
+# machine, so visit http://www.crummy.com/software/BeautifulSoup/bs4/doc/ for
+# more information.
+
 from bs4 import BeautifulSoup
 import telebot
 import urllib
 import os
 import sys
 
+# Initialize the bot with its token. Remember to replace bot_token_here with
+# your bot token. You need to talk to BotFather to get one.
+
 bot = telebot.TeleBot("bot_token_here")
+
+# Download the data we need and save it to a list of strings.
+
 url = 'http://scu.ugr.es/'
 try:
     page = BeautifulSoup(urllib.request.urlopen(url), "html.parser")
@@ -14,6 +28,8 @@ except urllib.error.HTTPError as e:
     sys.exit(error_message)
 
 menu_list = [elem.get_text() for elem in data]
+
+# Create several lists for every day and parse the data for each one.
 
 index = 0
 lunes = []
@@ -42,15 +58,21 @@ while (index < len(menu_list)):
     sabado.append(menu_list[index])
     index += 1
 
+# Handler for command start. It returns a welcome message to the chat.
+
 @bot.message_handler(commands=['start'])
 def welcome_message(message):
     msg = '¡Ya estamos listos para empezar! Escribe /diadelasemana para obtener el menú correspondiente. También puedes obtener el menú semanal en pdf usando /pdf. Si necesitas ayuda, usa /help.'
     bot.send_message(message.chat.id, msg)
 
+# Handler for help command. It returns some help to the user.
+
 @bot.message_handler(commands=['help'])
 def welcome_message(message):
     msg = 'Si deseas obtener el menú de un día concreto, usa /lunes, /martes... etc. Además, escribiendo /pdf puedes obtener el documento pdf con el menú semanal completo.'
     bot.send_message(message.chat.id, msg)
+
+# Handler for every day commands. It prints the menu of the day the user asked for.
 
 @bot.message_handler(commands=['lunes','martes','miercoles','jueves','viernes','sabado'])
 def print_menu(message):
@@ -78,6 +100,9 @@ def print_menu(message):
         for menu in sabado:
             bot.send_message(message.chat.id, menu)
 
+# Handler for pdf command. It downloads the document with the full menu and
+# returns it to the user chat.
+
 @bot.message_handler(commands=['pdf'])
 def print_menu(message):
     try:
@@ -94,5 +119,8 @@ def print_menu(message):
         error_message = "Error %s. Prueba de nuevo en unos minutos." % e.code
         bot.send_message(message.chat.id, error_message)
         os.remove(pdf_filename)
+
+# This function keeps the connection alive to the Telegram Bot API and does all
+# the neccesary operations to send data.
 
 bot.polling()
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # ComedoresUGRbot
 Telegram bot to check the menu of Universidad de Granada dining hall. It works
-thanks to <a href="https://github.com/eternnoir/pyTelegramBotAPI/">pyTelegramBotAPI</a>.
+thanks to <a href="https://github.com/eternnoir/pyTelegramBotAPI/">pyTelegramBotAPI</a>
+and <a href="http://www.crummy.com/software/BeautifulSoup/">BeautifulSoup</a>.
 
 This bot is available at: http://telegram.me/ComedoresUGRbot
 
@@ -16,6 +17,7 @@ Once you have installed all the requirements with pip, then add your bot token t
 
 > python3 ComedoresUGRbot.py
 
-All should work okay.
+All should work okay. **This bot has to be restarted manually every week**, once
+the menu is updated on http://scu.ugr.es.
 
 This bot is licensed under GPLv3. See LICENSE file for more information.
