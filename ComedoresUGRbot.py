19dba87ff0dd715526ca8970d26a186e2faa02e7
Alejandro Castilla Quesada <contacto@alejandro-castilla.com>
2016-11-29 21:44:20 +0100

    Add today command and some refactoring

--- ComedoresUGRbot.py
+++ ComedoresUGRbot.py
@@ -8,12 +8,13 @@ from bs4 import BeautifulSoup
 import telebot
 import urllib
 import os
+import datetime
 import sys
 
 # Initialize the bot with its token. Remember to replace bot_token_here with
 # your bot token. You need to talk to @BotFather to get one.
 
-bot = telebot.TeleBot("bot_token_here")
+bot = telebot.TeleBot("BOT_TOKEN_HERE")
 
 # URLs and filename that we need to work.
 url = 'http://scu.ugr.es/'
@@ -22,110 +23,141 @@ pdf_filename = 'menu_comedores.pdf'
 
 # Download the pdf and save it to a file.
 try:
-    f = open(pdf_filename,'wb')
-    f.write(urllib.request.urlopen(url_pdf).read())
-    f.close()
+	f = open(pdf_filename,'wb')
+	f.write(urllib.request.urlopen(url_pdf).read())
+	f.close()
 except IOError as e:
-    os.remove(pdf_filename)
-    sys.exit(e)
+	os.remove(pdf_filename)
+	sys.exit(e)
 except urllib.error.HTTPError as e:
-    os.remove(pdf_filename)
-    error_message = "Error %s HTTP." % e.code
-    sys.exit(error_message)
+	os.remove(pdf_filename)
+	error_message = "Error %s HTTP." % e.code
+	sys.exit(error_message)
 
 # Download the web page and save the table we need. Then we create a list of strings.
 try:
-    page = BeautifulSoup(urllib.request.urlopen(url), "html.parser")
-    data = page.find("table").find_all("td")
+	page = BeautifulSoup(urllib.request.urlopen(url), "html.parser")
+	data = page.find("table").find_all("td")
 except urllib.error.HTTPError as e:
-    error_message = "Error %s HTTP." % e.code
-    sys.exit(error_message)
+	error_message = "Error %s HTTP." % e.code
+	sys.exit(error_message)
 
 menu_list = [elem.get_text() for elem in data]
 
 # Create several lists for every day and parse the data for each one.
 
 index = 0
-lunes = []
-martes = []
-miercoles = []
-jueves = []
-viernes = []
-sabado = []
+monday = []
+tuesday = []
+wednesday = []
+thursday = []
+friday = []
+saturday = []
 
 while ("MARTES" not in menu_list[index]):
-    lunes.append(menu_list[index])
-    index += 1
+	monday.append(menu_list[index])
+	index += 1
 while ("MIÉRCOLES" not in menu_list[index]):
-    martes.append(menu_list[index])
-    index += 1
+	tuesday.append(menu_list[index])
+	index += 1
 while ("JUEVES" not in menu_list[index]):
-    miercoles.append(menu_list[index])
-    index += 1
+	wednesday.append(menu_list[index])
+	index += 1
 while ("VIERNES" not in menu_list[index]):
-    jueves.append(menu_list[index])
-    index += 1
+	thursday.append(menu_list[index])
+	index += 1
 while ("SÁBADO" not in menu_list[index]):
-    viernes.append(menu_list[index])
-    index += 1
+	friday.append(menu_list[index])
+	index += 1
 while (index < len(menu_list)):
-    sabado.append(menu_list[index])
-    index += 1
+	saturday.append(menu_list[index])
+	index += 1
+
+def print_monday(chat_id):
+	for menu in monday:
+		bot.send_message(chat_id, menu)
+def print_tuesday(chat_id):
+	for menu in tuesday:
+		bot.send_message(chat_id, menu)
+def print_wednesday(chat_id):
+	for menu in wednesday:
+		bot.send_message(chat_id, menu)
+def print_thursday(chat_id):
+	for menu in thursday:
+		bot.send_message(chat_id, menu)
+def print_friday(chat_id):
+	for menu in friday:
+		bot.send_message(chat_id, menu)
+def print_saturday(chat_id):
+	for menu in saturday:
+		bot.send_message(chat_id, menu)
+
 
 # Handler for command start. It returns a welcome message to the chat.
 
 @bot.message_handler(commands=['start'])
 def welcome_message(message):
-    msg = '¡Ya estamos listos para empezar! Escribe /diadelasemana para obtener el menú correspondiente. También puedes obtener el menú semanal en pdf usando /pdf. Si necesitas ayuda, usa /help.'
-    bot.send_message(message.chat.id, msg)
+	msg = '¡Ya estamos listos para empezar! Escribe /diadelasemana o /hoy para obtener el menú correspondiente. También puedes obtener el menú semanal en pdf usando /pdf. Si necesitas ayuda, usa /help.'
+	bot.send_message(message.chat.id, msg)
 
 # Handler for help command. It returns some help to the user.
 
 @bot.message_handler(commands=['help'])
 def help_message(message):
-    msg = 'Si deseas obtener el menú de un día concreto, usa /lunes, /martes... etc. Además, escribiendo /pdf puedes obtener el documento pdf con el menú semanal completo.'
-    bot.send_message(message.chat.id, msg)
+	msg = 'Si deseas obtener el menú de un día concreto, usa /lunes, /martes... o /hoy . Además, escribiendo /pdf puedes obtener el documento pdf con el menú semanal completo.'
+	bot.send_message(message.chat.id, msg)
 
 # Handler for every day commands. It prints the menu of the day the user asked for.
 
 @bot.message_handler(commands=['lunes','martes','miercoles','jueves','viernes','sabado'])
 def print_menu(message):
-    if (message.text.startswith("/lunes")):
-        for menu in lunes:
-            bot.send_message(message.chat.id, menu)
+	chat_id = message.chat.id
+	
+	if (message.text.startswith("/lunes")):
+		print_monday(chat_id)
+	if (message.text.startswith("/martes")):
+		print_tuesday(chat_id)
+	if (message.text.startswith("/miercoles")):
+		print_wednesday(chat_id)
+	if (message.text.startswith("/jueves")):
+		print_thursday(chat_id)
+	if (message.text.startswith("/viernes")):
+		print_friday(chat_id)
+	if (message.text.startswith("/sabado")):
+		print_saturday(chat_id)
 
-    if (message.text.startswith("/martes")):
-        for menu in martes:
-            bot.send_message(message.chat.id, menu)
+# Handler for today command
 
-    if (message.text.startswith("/miercoles")):
-        for menu in miercoles:
-            bot.send_message(message.chat.id, menu)
+@bot.message_handler(commands=['hoy'])
+def print_menu_today(message):
+	chat_id = message.chat.id
+	week_day = datetime.date.today().weekday() # 0 is monday and 6 is sunday
 
-    if (message.text.startswith("/jueves")):
-        for menu in jueves:
-            bot.send_message(message.chat.id, menu)
-
-    if (message.text.startswith("/viernes")):
-        for menu in viernes:
-            bot.send_message(message.chat.id, menu)
-
-    if (message.text.startswith("/sabado")):
-        for menu in sabado:
-            bot.send_message(message.chat.id, menu)
+	if (week_day == 0):
+		print_monday(chat_id)
+	if (week_day == 1):
+		print_tuesday(chat_id)
+	if (week_day == 2):
+		print_wednesday(chat_id)
+	if (week_day == 3):
+		print_thursday(chat_id)
+	if (week_day == 4):
+		print_friday(chat_id)
+	if (week_day == 5):
+		print_saturday(chat_id)
 
 # Handler for pdf command. It downloads the document with the full menu and
 # returns it to the user chat.
 
 @bot.message_handler(commands=['pdf'])
 def send_pdf(message):
-    try:
-        doc = open(pdf_filename, 'rb')
-        bot.send_document(message.chat.id, doc)
-        doc.close()
-    except IOError as e:
-        sys.exit(e)
-        os.remove(pdf_filename)
+	try:
+		doc = open(pdf_filename, 'rb')
+		bot.send_document(message.chat.id, doc)
+		doc.close()
+	except IOError as e:
+		sys.exit(e)
+		os.remove(pdf_filename)
 
 # This function keeps the connection to the Telegram Bot API alive and does all
 # the neccesary operations to send the data. none_stop=True prevents the script
