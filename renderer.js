c8119eccc976a7b29becd2c098656bc1836e4586
Alejandro Castilla <contacto@alejandro-castilla.com>
2017-11-06 21:59:49 +0100

    Changes in renderer.js to support new scu.ugr.es DOM changes.
    Now only last div.level2 tables are considered valid, because sometimes tables
    of the previous week are shown in the first div.level2 DOM element.

--- renderer.js
+++ renderer.js
@@ -1,33 +1,42 @@
-var casper = require('casper').create();
+var casper = require('casper').create({
+  verbose: true,
+  logLevel: 'info'
+});
 
 var days = [
-	'monday',
-	'tuesday',
-	'wednesday',
-	'thursday',
-	'friday',
-	'saturday'
+  'monday',
+  'tuesday',
+  'wednesday',
+  'thursday',
+  'friday',
+  'saturday'
 ];
 
 casper.start('http://scu.ugr.es', function() {
-  this.echo('Loading web page: ' + this.getCurrentUrl());
+  casper.log('Loading web page: ' + this.getCurrentUrl(), 'info');
+});
+
+casper.on("page.error", function(msg, trace) {
+  casper.log(msg, 'error');
 });
 
 casper.then(function() {
   this.waitForSelector('.inline', function() {
-		var rects = this.evaluate(function() {
-			var result = [];
-			var tables = document.querySelectorAll('.inline');
+    var rects = this.evaluate(function() {
+      var result = [];
+      var containers = document.querySelectorAll('div.level2');
+      var tables = containers[containers.length - 1]
+        .querySelectorAll('.inline');
 
-			for (var i = 0; i < tables.length; i++) {
-				result.push(tables[i].getBoundingClientRect());
-			}
+      for (var i = 0; i < tables.length; i++) {
+        result.push(tables[i].getBoundingClientRect());
+      }
 
-			return result;
-		});
+      return result;
+    });
 
-		// Render only tables with menu of each day
-		for (var i = 0; i < days.length; i++) {
+    // Render only tables with menu of each day
+    for (var i = 0; i < days.length; i++) {
       var tableRect = rects[i];
       this.capture('images/' + days[i] + '.png', {
         top: tableRect.top,
@@ -37,8 +46,8 @@ casper.then(function() {
       });
     }
 
-		this.echo('Images rendered successfully')
-  })
+    casper.log('Images rendered successfully', 'info');
+  });
 });
 
 casper.run();
